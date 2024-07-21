4b6559d0c3fa352a639bfaab1854c99f4a0b85db
ZinetsVO <zinets.vo@gmail.com>
2024-07-21 10:15:47 +0300

    layout reorgainzed

---
+++ src/app/(moderate)/admin/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AdminLayout: React.FC = () => {
+
+    //verification
+    // input if auth == false
+  return (
+    <div>AdminLayout</div>
+  )
+}
+
+export default AdminLayout
