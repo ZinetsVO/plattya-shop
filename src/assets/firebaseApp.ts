99486e1a820d2bfb837a4f229f4ff80355c1f641
ZinetsVO <zinets.vo@gmail.com>
2024-07-28 11:16:07 +0300

    env added

--- src/assets/firebaseApp.ts
+++ src/assets/firebaseApp.ts
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCDc4Y8oIgRha4GPu8FZbEGmQ2K-2DPLw",
-  authDomain: "plattya-shop.firebaseapp.com",
-  projectId: "plattya-shop",
-  storageBucket: "plattya-shop.appspot.com",
-  messagingSenderId: "462438275368",
-  appId: "1:462438275368:web:5929b4b3e914dd8817181d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
-export { storage , app};
+export { storage, app };
